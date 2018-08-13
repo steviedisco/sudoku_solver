cef90339e352c864d7b59b2aca3003a24d1b641a
Steve Driscoll <steviedisco@gmail.com>
2018-08-13 11:13:18 +0100

    started working on png solver

--- main.nim
+++ main.nim
@@ -16,21 +16,29 @@ proc output_csv(output_dir: string, input_file: string, content: string) =
     module.write(content)
     module.close
 
-proc solve_csv(csv: string, output_dir: string) =
+proc process_csv(csv: string, output_dir: string) =
     var known = csv_parser.parse(csv)
     var solution = solver.solve(known)
     output_csv(output_dir, csv, matrix_convert.to_string(solution))
 
-proc process_csvs(input_dir: string, output_dir: string) =
-    var csv_inputs = list_files(input_dir, "csv")
+proc process_png(csv: string, output_dir: string) =
+    var known = csv_parser.parse(csv)
+    var solution = solver.solve(known)
+    output_csv(output_dir, csv, matrix_convert.to_string(solution))
 
-    for csv in csv_inputs:
-        echo time(csv, solve_csv(csv, output_dir))
+proc process(input_dir: string, output_dir: string, ext: string) =
+    var inputs = list_files(input_dir, ext)
+
+    for input in inputs:
+        if ext == "csv":
+            echo time(input, process_csv(input, output_dir)) 
+        else:
+            echo time(input, process_png(input, output_dir)) 
 
 proc main() =
     var input_dir = paramStr(1)
     var output_dir = paramStr(2)
-
-    echo time("All CSVs", process_csvs(input_dir, output_dir))
+    echo time("All CSVs", process(input_dir, output_dir, "csv"))
+    echo time("All PNGs", process(input_dir, output_dir, "png"))
 
 echo time("Total", main())
